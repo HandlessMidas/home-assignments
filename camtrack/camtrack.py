8c55dd6ca72fbb3d48a26c24c34d1bb4515c3124
handlessmidas <midas1024@yandex.ru>
2020-11-02 01:59:21 +0500

    Fixed

--- camtrack/camtrack.py
+++ camtrack/camtrack.py
@@ -63,8 +63,8 @@ def solve_pnp(frame, intrinsic_mat, corner_storage, point_cloud_builder, outlier
 def add_points(frame_1, frame_2, corner_storage, view_mats, point_cloud_builder, intrinsic_mat):
     triang_params = TriangulationParameters(4, 1, 0)
     cors = build_correspondences(
-        corner_storage[frame_1], corner_storage[frame_2], point_cloud_builder.ids)
-    if (len(cors) == 0):
+        corner_storage[frame_1], corner_storage[frame_2], ids_to_remove=point_cloud_builder.ids)
+    if (len(cors.ids) == 0):
         return
     points, ids, _ = triangulate_correspondences(
         cors, view_mats[frame_1], view_mats[frame_2], intrinsic_mat, triang_params)
@@ -80,9 +80,9 @@ def track_camera(corner_storage, intrinsic_mat, known_view_1, known_view_2):
     view_mat_2 = pose_to_view_mat3x4(known_view_2[1])
     view_mats[known_view_1[0]] = view_mat_1
     view_mats[known_view_2[0]] = view_mat_2
-    start_frame_1 = min(known_view_1[0], known_view_2[0])
-    start_frame_2 = max(known_view_1[0], known_view_2[0])
-    processing_frame = start_frame_1 + 1
+    known_frame_1 = min(known_view_1[0], known_view_2[0])
+    known_frame_2 = max(known_view_1[0], known_view_2[0])
+    processing_frame = known_frame_1 + 1
     outliers = np.zeros(
         max([i.ids.size for i in corner_storage]) + 1, dtype=bool)
     camera_centers = [to_camera_center(
@@ -90,37 +90,36 @@ def track_camera(corner_storage, intrinsic_mat, known_view_1, known_view_2):
     start_distance = np.linalg.norm(camera_centers[0] - camera_centers[1])
     last_frame = known_view_1[0]
 
-    add_points(last_frame, known_view_2[0], corner_storage,
+    add_points(known_frame_1, known_frame_2, corner_storage,
                view_mats, point_cloud_builder, intrinsic_mat)
 
     for _ in range(2, total_frames):
-        if processing_frame == start_frame_2 and processing_frame < total_frames - 1:
-            processing_frame += 1
-
-        if processing_frame >= total_frames:
-            continue
+        processing_frame += ((processing_frame ==
+                              known_frame_2) and (known_frame_2 != total_frames - 1))
 
         print(f'Processing {processing_frame}/{total_frames - 1} frame')
 
-        view_mats[processing_frame] = solve_pnp(
-            processing_frame, intrinsic_mat, corner_storage, point_cloud_builder, outliers, view_mats)
+        if processing_frame < total_frames:
+            view_mats[processing_frame] = solve_pnp(
+                processing_frame, intrinsic_mat, corner_storage, point_cloud_builder, outliers, view_mats)
 
-        counter = 0
-        last_frame_dist = 5
-        while counter < 5:
-            if processing_frame > start_frame_1:
-                last_frame = processing_frame - last_frame_dist
-            else:
-                last_frame = processing_frame + last_frame_dist
-            last_frame_dist += 1
-            if check_baseline(view_mats[last_frame], view_mats[processing_frame], start_distance * 0.1):
-                add_points(last_frame, processing_frame, corner_storage,
-                           view_mats, point_cloud_builder, intrinsic_mat)
-                counter += 1
-            else:
-                break
+            counter = 0
+            last_frame_dist = 3
+            while counter < 3:
+                if processing_frame > known_frame_1:
+                    last_frame = processing_frame - last_frame_dist
+                else:
+                    last_frame = processing_frame + last_frame_dist
+                last_frame_dist += 1
+                if last_frame < total_frames and (known_frame_1 <= last_frame or processing_frame < known_frame_1):
+                    if check_baseline(view_mats[last_frame], view_mats[processing_frame], start_distance * 0.1):
+                        add_points(last_frame, processing_frame, corner_storage,
+                                   view_mats, point_cloud_builder, intrinsic_mat)
+                        counter += 1
+                else:
+                    break
 
-        if processing_frame > start_frame_1:
+        if processing_frame > known_frame_1:
             processing_frame += 1
         else:
             processing_frame -= 1
