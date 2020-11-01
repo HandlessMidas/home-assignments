378a2319edfe5ce0505c5163401a233618b7f51d
handlessmidas <midas1024@yandex.ru>
2020-11-02 01:59:28 +0500

    HW2

--- camtrack/camtrack.py
+++ camtrack/camtrack.py
@@ -7,6 +7,7 @@ __all__ = [
 from typing import List, Optional, Tuple
 
 import numpy as np
+import cv2
 
 from corners import CornerStorage
 from data3d import CameraParameters, PointCloud, Pose
@@ -17,8 +18,114 @@ from _camtrack import (
     calc_point_cloud_colors,
     pose_to_view_mat3x4,
     to_opencv_camera_mat3x3,
-    view_mat3x4_to_pose
+    view_mat3x4_to_pose,
+    build_correspondences,
+    triangulate_correspondences,
+    TriangulationParameters,
+    rodrigues_and_translation_to_view_mat3x4,
+    eye3x4,
+    to_camera_center,
+    calc_inlier_indices,
+    check_baseline
 )
+
+
+def solve_pnp(frame, intrinsic_mat, corner_storage, point_cloud_builder, outliers, view_mats, time=0):
+    corners = corner_storage[frame]
+    inter, object_id, image_id = np.intersect1d(
+        point_cloud_builder.ids, corners.ids, return_indices=True)
+
+    for mask in [outliers, np.zeros_like(outliers)]:
+        ids = np.where(np.invert(mask))
+        inliers_ids = np.intersect1d(ids, inter, return_indices=True)[2]
+        image_ids = corners.ids[image_id[inliers_ids]]
+        image_points = corners.points[image_id[inliers_ids]]
+        object_points = point_cloud_builder.points[object_id[inliers_ids]]
+        if object_points.shape[0] >= 4:
+            break
+
+    _, rvec, tvec, inliers = cv2.solvePnPRansac(
+        objectPoints=object_points, imagePoints=image_points, cameraMatrix=intrinsic_mat, distCoeffs=None, reprojectionError=2, flags=cv2.SOLVEPNP_EPNP)
+    if time == 0:
+        triang_error = 4
+        while len(inliers) < 5 and triang_error < 100:
+            triang_error *= 1.03
+            inliers = calc_inlier_indices(object_points, image_points, np.matmul(
+                intrinsic_mat, view_mats[frame]), triang_error)
+        outlier_ids = np.setdiff1d(image_ids, image_ids[inliers])
+        outliers[outlier_ids] = True
+        solve_pnp(frame, intrinsic_mat, corner_storage,
+                  point_cloud_builder, outliers, view_mats, time=1)
+    print(f'Number of inliers: {len(inliers)}')
+    return rodrigues_and_translation_to_view_mat3x4(rvec, tvec)
+
+
+def add_points(frame_1, frame_2, corner_storage, view_mats, point_cloud_builder, intrinsic_mat):
+    triang_params = TriangulationParameters(4, 1, 0)
+    cors = build_correspondences(
+        corner_storage[frame_1], corner_storage[frame_2], point_cloud_builder.ids)
+    if (len(cors) == 0):
+        return
+    points, ids, _ = triangulate_correspondences(
+        cors, view_mats[frame_1], view_mats[frame_2], intrinsic_mat, triang_params)
+    point_cloud_builder.add_points(ids, points)
+    print(f'Point cloud size: {point_cloud_builder.points.size}')
+
+
+def track_camera(corner_storage, intrinsic_mat, known_view_1, known_view_2):
+    point_cloud_builder = PointCloudBuilder()
+    total_frames = len(corner_storage)
+    view_mats = [eye3x4()] * total_frames
+    view_mat_1 = pose_to_view_mat3x4(known_view_1[1])
+    view_mat_2 = pose_to_view_mat3x4(known_view_2[1])
+    view_mats[known_view_1[0]] = view_mat_1
+    view_mats[known_view_2[0]] = view_mat_2
+    start_frame_1 = min(known_view_1[0], known_view_2[0])
+    start_frame_2 = max(known_view_1[0], known_view_2[0])
+    processing_frame = start_frame_1 + 1
+    outliers = np.zeros(
+        max([i.ids.size for i in corner_storage]) + 1, dtype=bool)
+    camera_centers = [to_camera_center(
+        view_mat_1), to_camera_center(view_mat_2)]
+    start_distance = np.linalg.norm(camera_centers[0] - camera_centers[1])
+    last_frame = known_view_1[0]
+
+    add_points(last_frame, known_view_2[0], corner_storage,
+               view_mats, point_cloud_builder, intrinsic_mat)
+
+    for _ in range(2, total_frames):
+        if processing_frame == start_frame_2 and processing_frame < total_frames - 1:
+            processing_frame += 1
+
+        if processing_frame >= total_frames:
+            continue
+
+        print(f'Processing {processing_frame}/{total_frames - 1} frame')
+
+        view_mats[processing_frame] = solve_pnp(
+            processing_frame, intrinsic_mat, corner_storage, point_cloud_builder, outliers, view_mats)
+
+        counter = 0
+        last_frame_dist = 5
+        while counter < 5:
+            if processing_frame > start_frame_1:
+                last_frame = processing_frame - last_frame_dist
+            else:
+                last_frame = processing_frame + last_frame_dist
+            last_frame_dist += 1
+            if check_baseline(view_mats[last_frame], view_mats[processing_frame], start_distance * 0.1):
+                add_points(last_frame, processing_frame, corner_storage,
+                           view_mats, point_cloud_builder, intrinsic_mat)
+                counter += 1
+            else:
+                break
+
+        if processing_frame > start_frame_1:
+            processing_frame += 1
+        else:
+            processing_frame -= 1
+
+    return view_mats, point_cloud_builder
 
 
 def track_and_calc_colors(camera_parameters: CameraParameters,
@@ -36,12 +143,8 @@ def track_and_calc_colors(camera_parameters: CameraParameters,
         rgb_sequence[0].shape[0]
     )
 
-    # TODO: implement
-    frame_count = len(corner_storage)
-    view_mats = [pose_to_view_mat3x4(known_view_1[1])] * frame_count
-    corners_0 = corner_storage[0]
-    point_cloud_builder = PointCloudBuilder(corners_0.ids[:1],
-                                            np.zeros((1, 3)))
+    view_mats, point_cloud_builder = track_camera(
+        corner_storage, intrinsic_mat, known_view_1, known_view_2)
 
     calc_point_cloud_colors(
         point_cloud_builder,
--- camtrack/corners.py
+++ camtrack/corners.py
@@ -34,26 +34,134 @@ class _CornerStorageBuilder:
         return StorageImpl(item[1] for item in sorted(self._corners.items()))
 
 
+def _to_np_int8(arr):
+    return (255 * arr).astype(np.uint8)
+
+
+class CornerTracker:
+
+    def __init__(self, frame):
+        self.frame = frame
+        self.max_corners = 10000
+        self.quality = 0.008
+        self.min_dist = 4
+        self.corners, self.radiuses = self.find_corners(
+            frame, max_corners=self.max_corners, quality=0.002)
+        self.corner_ids = np.arange(self.corners.shape[0])
+        self.last_id = self.corner_ids.shape[0]
+
+    def process_frame(self, new_frame):
+        self.corners, mask = self.apply_opt_flow(new_frame, self.corners)
+        self.corners = self.corners[mask]
+        self.radiuses = self.radiuses[mask]
+        self.corner_ids = self.corner_ids[mask]
+        filter_ = self.create_filter(new_frame, self.corners, self.min_dist)
+        if self.corners.shape[0] < self.max_corners:
+            new_corners, new_radiuses = self.find_corners(new_frame, filter_,
+                                                          max_corners=self.max_corners -
+                                                          self.corners.shape[0],
+                                                          quality=self.quality)
+            new_ids = np.arange(
+                self.last_id, self.last_id + new_corners.shape[0])
+            self.last_id += new_ids.shape[0]
+            self.corners = np.concatenate([self.corners, new_corners])
+            self.radiuses = np.concatenate([self.radiuses, new_radiuses])
+            self.corner_ids = np.concatenate([self.corner_ids, new_ids])
+
+        mask = self.remove_close_corners(self.corners, self.min_dist)
+        self.corners = self.corners[mask == 1]
+        self.radiuses = self.radiuses[mask == 1]
+        self.corner_ids = self.corner_ids[mask == 1]
+
+        self.frame = new_frame
+
+    def find_corners(self, frame, filter_=None, pyr_level=3, point_size=4, max_corners=10000, quality=0.008,
+                     min_dist=4, block_size=7):
+        corners = np.empty((0, 2)).astype(np.float32)
+        radiuses = np.empty(0)
+        coef = 1
+        frame_ = frame.copy()
+        for i in range(pyr_level):
+            if filter_ is not None:
+                filter_[filter_ != 255] = 0
+            new_corners = cv2.goodFeaturesToTrack(_to_np_int8(frame_), max_corners, quality, min_dist * coef,
+                                                  mask=filter_, blockSize=block_size)
+            if new_corners is None:
+                new_corners = np.empty((0, 2)).astype(np.float32)
+            new_corners *= coef
+            new_corners = new_corners.reshape(new_corners.shape[0], 2)
+            new_radiuses = np.ones(new_corners.shape[0]) * point_size * coef
+            corners = np.concatenate([corners, new_corners])
+            radiuses = np.concatenate([radiuses, new_radiuses])
+            new_filter = self.create_filter(
+                frame_, new_corners / coef, area_size=min_dist)
+            frame_ = cv2.pyrDown(frame_)
+            if filter_ is not None:
+                filter_mask = np.where(new_filter == 0)
+                filter_[filter_mask] = 0
+                filter_ = cv2.pyrDown(filter_)
+            else:
+                filter_ = new_filter
+                filter_ = cv2.pyrDown(filter_)
+            coef *= 2
+            max_corners = max(0, max_corners - new_corners.shape[0])
+            if max_corners == 0:
+                return corners, radiuses
+        return corners, radiuses
+
+    def create_filter(self, image, corners, area_size=4):
+        filter_ = np.full(image.shape, 255).astype(np.uint8)
+        for p in corners:
+            filter_ = cv2.circle(filter_, (p[0], p[1]), area_size, 0, -1)
+        return filter_
+
+    def apply_opt_flow(self, new_frame, corners, max_level=2):
+        p1, st1, _ = cv2.calcOpticalFlowPyrLK(_to_np_int8(self.frame), _to_np_int8(new_frame),
+                                              corners, None, maxLevel=max_level)
+        pb, stb, _ = cv2.calcOpticalFlowPyrLK(_to_np_int8(new_frame), _to_np_int8(self.frame),
+                                              p1, None, maxLevel=max_level)
+        dists = abs(corners - pb).squeeze().max(axis=1) < 0.4
+        mask = dists & (st1 == 1).squeeze()
+        return p1, mask
+
+    def remove_close_corners(self, corners, min_dist):
+        good_corners = np.array([corners[0]])
+        st = np.ones(corners.shape[0])
+        for i in range(corners.shape[0]):
+            p = corners[i]
+            c = np.min(np.sum(([p] - good_corners) ** 2, axis=1))
+            if np.sqrt(c) > min_dist:
+                good_corners = np.concatenate([good_corners, [p]])
+            else:
+                st[i] = 0
+        return st
+
+    def get_corners(self):
+        return self.corners, self.corner_ids, self.radiuses
+
+
 def _build_impl(frame_sequence: pims.FramesSequence,
                 builder: _CornerStorageBuilder) -> None:
-    # TODO
+
     image_0 = frame_sequence[0]
-    corners = FrameCorners(
-        np.array([0]),
-        np.array([[0, 0]]),
-        np.array([55])
-    )
-    builder.set_corners_at_frame(0, corners)
+
+    corner_tracker = CornerTracker(image_0)
+    corners, corners_ids, radiuses = corner_tracker.get_corners()
+    builder.set_corners_at_frame(
+        0, FrameCorners(corners_ids, corners, radiuses))
+
     for frame, image_1 in enumerate(frame_sequence[1:], 1):
-        builder.set_corners_at_frame(frame, corners)
-        image_0 = image_1
+
+        corner_tracker.process_frame(image_1)
+        corners, corners_ids, radiuses = corner_tracker.get_corners()
+        builder.set_corners_at_frame(
+            frame, FrameCorners(corners_ids, corners, radiuses))
 
 
 def build(frame_sequence: pims.FramesSequence,
           progress: bool = True) -> CornerStorage:
     """
     Build corners for all frames of a frame sequence.
-
     :param frame_sequence: grayscale float32 frame sequence.
     :param progress: enable/disable building progress bar.
     :return: corners for all frames of given sequence.
